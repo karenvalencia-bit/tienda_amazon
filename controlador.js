f1a7a7f271f59fab2004986b19fa4217e8fb9be5
karenvalencia-bit <karenochoa196@gmail.com>
2021-08-09 15:39:58 -0500

    jueves

--- controlador.js
+++ controlador.js
@@ -1,3 +1,9 @@
+//CONTROLAR LA ACTIVIDAD DEL FORMULARIO
+let nombreConsola
+let precioConsola
+let pesoConsola
+let fotoConsola
+
 //boton del formulario 
 let boton=document.getElementById("boton");
 console.log(boton);
@@ -7,15 +13,30 @@ console.log(boton);
 boton.addEventListener("click", capturarDatos);//calback
 
 //3.CREAR FUNCION PROPIA QUE SE LLAMO 2
-function capturardatos(){
+function capturarDatos(){
     
     //4. capturar los valores del formulario
 
     let consolaElegida=document.getElementById("productos").value;
     seleccionarConsola(consolaElegida);
-    console.log(nombreConsola);
-    console.log(precioConsola);
-    console.log(pesoConsola);
+    
+    let cantidad=document.getElementById("cantidad").value;
+    console.log(cantidad);
+
+    let pildora=document.getElementById("pildora");
+    pildora.textContent=cantidad;
+    console.log(pildora.textContent);
+    pildora.classList.remove("invisible")
+    pildora.classList.add("visible")
+
+    let direccion=document.getElementById("comentarios").value;
+    console.log(direccion);
+
+    let fotoCarrito=document.getElementById("fotocarrito");
+    fotoCarrito.src=fotoConsola;
+
+    let tituloCarrito=document.getElementById("tituloCarrito");
+    tituloCarrito.textContent=nombreConsola;
 
 }
  
@@ -23,7 +44,8 @@ function seleccionarConsola(opcion){
 let consolas={ 
     nombres:Array("PS5","XBOX Serie X", "Nintendo Switch"),
      precios:Array(750,700,409), 
-     pesos:Array(14.2,13.1,4.75)
+     pesos:Array(14.2,13.1,4.75),
+     fotos:Array("https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/ps5.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/xbox.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/nintendo.jpg?raw=true")
      }
 
      
@@ -32,23 +54,28 @@ let consolas={
         nombreConsola=consolas.nombres[0];
         precioConsola=consolas.precios[0];
         pesoConsola=consolas.pesos[0];
+        fotoConsola=consolas.fotos[0];
 
     }else if(opcion==2){
 
         nombreConsola=consolas.nombres[1];
         precioConsola=consolas.precios[1];
         pesoConsola=consolas.pesos[1];
+        fotoConsola=consolas.fotos[1];
 
     }else if(opcion==3){
 
         nombreConsola=consolas.nombres[2];
         precioConsola=consolas.precios[2];
         pesoConsola=consolas.pesos[2];
+        fotoConsola=consolas.fotos[2];
 
     }else{
         nombreConsola=null;
         precioConsola=null;
         pesoConsola=null;
+        fotoConsola=null
+
 
     }
 
